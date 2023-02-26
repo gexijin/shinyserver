5977f2da98bf1bf3bc82defbfdf1127ba38946e9
Steven Ge <xijin.ge@sdstate.edu>
2023-02-26 15:41:01 -0600

    server config

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 echo $(date) >> log.txt
 
 # shut down
-sudo docker-compose -f /home/ubuntu/RTutor_server/docker-compose.yml down
+sudo docker-compose  down
 
 # restart with multiple containers.
-sudo docker-compose -f /home/ubuntu/RTutor_server/docker-compose.yml up -d --scale webapp=30
+sudo docker-compose up -d --scale webapp=30
